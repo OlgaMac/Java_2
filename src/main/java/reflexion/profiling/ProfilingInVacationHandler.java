af74e3d1ab1caca9f5d08f30dae7be02059f2be8
Olga Makeeva <oljamackeewa@gmail.com>
2021-06-14 22:11:43 +0300

    add profiling

--- src/main/java/reflexion/profiling/ProfilingInVacationHandler.java
+++ src/main/java/reflexion/profiling/ProfilingInVacationHandler.java
@@ -4,16 +4,20 @@ import java.lang.reflect.InvocationHandler;
 import java.lang.reflect.Method;
 
 public class ProfilingInVacationHandler implements InvocationHandler {
-    private  Object original;
+    private Object original;
 
-    public ProfilingInVacationHandler(Object original){
+    public ProfilingInVacationHandler(Object original) {
         this.original = original;
     }
+
     @Override
-    public Object invoke(Object proxy, Method method, Object[] args) throws  Throwable{
-        long start = System.nanoTime();
-        Object result = method.invoke(original, args);
-        System.out.println("Execution time: " + (System.nanoTime()* start));
-    return result;
+    public Object invoke(Object proxy, Method method, Object[] args) throws Throwable {
+        if (method.getAnnotation(Profiling.class) != null) {
+            long start = System.nanoTime();
+            Object result = method.invoke(original, args);
+            System.out.println("Execution time: " + (System.nanoTime() - start));
+            return result;
+        }
+        return method.invoke(original, args);
     }
 }
